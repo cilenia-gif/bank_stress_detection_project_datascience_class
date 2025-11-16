2fbc52b0aa647165f0d226ff86ce7a2c633ff639
cilenia-gif <clara@engeli-gaillat.ch>
2025-11-16 14:50:00 +0000

    Refactor repo for stress detection project

--- run.py
+++
@@ -1,5 +0,0 @@
-from src.bank_run_simulation.simulation import Simulation
-
-if __name__ == "__main__":
-    sim = Simulation()
-    sim.run()
--- src/bank_run_simulation/simulation.py
+++
@@ -1,8 +0,0 @@
-class Simulation:
-    def __init__(self, n_banks=5, steps=3):
-        self.n_banks = n_banks
-        self.steps = steps
-
-    def run(self):
-        print(f"Running simulation with {self.n_banks} banks for {self.steps} steps")
-        return list(range(self.steps))
